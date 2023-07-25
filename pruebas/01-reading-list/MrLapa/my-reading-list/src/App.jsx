312ab3e28c1ea6e775b827690d01c08614cbf54e
Carlos Garcia <carlos.garcia@boatyardx.com>
2023-07-25 18:28:33 -0500

    Arrange components and books lists in the main page
    The main page displays components and elements in their right place

--- pruebas/01-reading-list/MrLapa/my-reading-list/src/App.jsx
+++ pruebas/01-reading-list/MrLapa/my-reading-list/src/App.jsx
@@ -1,10 +1,29 @@
-import "./App.css";
+/* eslint-disable no-unused-vars */
+import classes from "./App.module.css";
+
+import Filters from "./components/Filters/Filters";
+import Header from "./components/Header/Header";
+
+import { library } from "../../../books.json";
+import BooksList from "./components/BooksList/BooksList";
 
 function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <main className={classes["main-wrapper"]}>
+      <section className={classes["available-books"]}>
+        <section className={classes["header-wrapper"]}>
+          <Header availableBooks={100} selectedBooks={7} />
+          <Filters defaultValues={{ pages: 300, category: "Zombies" }} />
+        </section>
+        <BooksList data={library} isAddButtonEnabled={true} />
+      </section>
+      {true && (
+        <aside className={classes["selected-books"]}>
+          <h2 className={classes.title}>Reading list</h2>
+          <BooksList data={library} isRemoveButtonEnabled={true} />
+        </aside>
+      )}
+    </main>
   );
 }
 
