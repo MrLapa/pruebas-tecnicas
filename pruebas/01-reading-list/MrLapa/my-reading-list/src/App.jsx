9a277022f234662abc3a9e8433d87dae0731d0de
Carlos Garcia <carlos.garcia@boatyardx.com>
2023-07-26 14:48:36 -0500

    Add custom filters hook to the Filters component and main page
    This hook will make the filters work on the main page

--- pruebas/01-reading-list/MrLapa/my-reading-list/src/App.jsx
+++ pruebas/01-reading-list/MrLapa/my-reading-list/src/App.jsx
@@ -3,24 +3,25 @@ import classes from "./App.module.css";
 
 import Filters from "./components/Filters/Filters";
 import Header from "./components/Header/Header";
-
-import { library } from "../../../books.json";
 import BooksList from "./components/BooksList/BooksList";
+import useFilters from "./hooks/useFilters";
 
 function App() {
+  const { selectedBooks } = useFilters();
+
   return (
     <main className={classes["main-wrapper"]}>
       <section className={classes["available-books"]}>
         <section className={classes["header-wrapper"]}>
           <Header availableBooks={100} selectedBooks={7} />
-          <Filters defaultValues={{ pages: 300, category: "Zombies" }} />
+          <Filters />
         </section>
-        <BooksList data={library} isAddButtonEnabled={true} />
+        <BooksList data={selectedBooks} isAddButtonEnabled={true} />
       </section>
       {true && (
         <aside className={classes["selected-books"]}>
           <h2 className={classes.title}>Reading list</h2>
-          <BooksList data={library} isRemoveButtonEnabled={true} />
+          <BooksList data={selectedBooks} isRemoveButtonEnabled={true} />
         </aside>
       )}
     </main>
